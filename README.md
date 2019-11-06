60e73b88e51a757bd1f080182334dc8ebf85093e
Aenik Nur Aufa <aeniknuraufa@users.noreply.github.com>
2019-11-07 00:35:21 +0700

    PBO2-10118082-Latihan41
     Membuat Program untuk menghitung massa jenis kubus berbasis objek

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# PBO2-10118082-Latihan41MassaJenis
-  Membuat Program untuk menghitung massa jenis kubus berbasis objek
+# PBO2-10118052-Latihan41-MassaJenis
+
+```java
+/**
+ *
+ * @author
+ * NAMA     : Bagus Syibro Malisi
+ * KELAS    : IF-2
+ * NIM      : 10118052
+ * Deskripsi Program : Membuat Program untuk menghitung massa jenis kubus berbasis
+ * object
+ */
+ ```
+ 
---
+++ src/pbo2/pkg10118082/latihan41/massajenis/Kubus.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118082.latihan41.massajenis;
+
+/**
+ *
+ * @author
+ * Nama     : Aenik Nur Aufa
+ * Kelas    : IF 2
+ * Nim      : 10118082
+ * Deskripsi Program : Membuat Program untuk menghitung massa jenis kubus berbasis
+ * object
+ */
+public class Kubus {
+    private int sisi, massa;
+
+    public int getSisi() {
+        return sisi;
+    }
+
+    public void setSisi(int sisi) {
+        this.sisi = sisi;
+    }
+
+    public int getMassa() {
+        return massa;
+    }
+
+    public void setMassa(int massa) {
+        this.massa = massa;
+    }
+    
+    public int hitungVolume(int parSisi) {
+        return (int)Math.pow(sisi, 3);
+    }
+    
+    public int hitungMassaJenis(int parMassa, int volume) {
+        return parMassa / volume;
+    }
+}
